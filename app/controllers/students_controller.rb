66140a0e1d40580b05e95684e187bf2d78f7dbe5
JackBidwell <jackbidwell2002@gmail.com>
2024-04-16 11:41:24 -0600

    Skills through capstone for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,7 @@ class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :update, :destroy]
 
   def index
-    @students = Student.all
+    @students = Student.includes(:experiences, :educations, :skills, :capstones).all
     render :index
   end
 
